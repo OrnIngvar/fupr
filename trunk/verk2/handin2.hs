5f77fb50b2dd40aaf03bd483bb0cd73fd046d793
davidhalldor <davidhalldor@88b713be-d0f2-422b-75c0-ee750841dce1>
2012-05-29 20:42:08 +0000

    Exercise 1

--- trunk/verk2/handin2.hs
+++ trunk/verk2/handin2.hs
@@ -2,4 +2,23 @@
 -- Handin 1
 
 -- Exercise 1
--- a) 
+-- a) Redefine the following version of the disjunction operator using conditional
+-- expressions rather than pattern matching:
+-- False || False = False
+-- _ || _ = True
+
+-- b) Redefine the following version of the disjunction operator using conditional
+-- expressions rather than pattern matching:
+-- False || b = b
+-- True || _ = True
+
+-- c) Define the function myTakeWhile which behaves like takeWhile, except
+-- that it is defined using conditional expressions only.
+
+-- d) Define the function myElem which behaves just like the function elem.
+-- Define it using
+-- i) Conditional expressions only (call this version myElem1),
+-- ii) Pattern matching and conditionals only (call this version myElem2),
+-- iii) Guards and pattern matching (call this version myElem3),
+-- iv) foldl or foldr, without using conditionals, pattern matching, guards,
+-- or recursion (call this version myElem4)
