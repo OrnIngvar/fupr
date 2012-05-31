01122c3c029f01e79963d02d7702ddae9eb7f8a6
orn@skynvilla.is <orn@skynvilla.is@88b713be-d0f2-422b-75c0-ee750841dce1>
2012-05-31 21:48:19 +0000

    bætti inn æfingum 2 og 3 (incomplete)

--- trunk/verk2/handin2.hs
+++ trunk/verk2/handin2.hs
@@ -32,3 +32,63 @@ x `or_operator'` y = if x == False
 -- iii) Guards and pattern matching (call this version myElem3),
 -- iv) foldl or foldr, without using conditionals, pattern matching, guards,
 -- or recursion (call this version myElem4)
+
+-- 2a)
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+-- [3,2,8,6,1,5,4,7]
+-- on first run of quicksort smallerSorted becomes :
+-- [ 2 ] quicksort [ 8,6,1,5,4,7 ] 
+-- second run biggerSorted becomes : 
+-- [ 8 ] quicksort [ 6,1,5,4,7 ]
+-- third run biggerSorted becomes :
+-- [ 8, 6 ] quicksort [ 1, 5, 4, 7 ]
+-- fourth run smallerSorted becomes :
+-- [ 2, 1 ] quicksort [ 5, 4, 7 ]
+-- fifth run biggerSorted becomes :
+-- [ 8, 6, 5 ] quicksort [ 4, 7 ]
+-- sixth run biggerSorted becomes :
+-- [ 8, 6, 5, 4 ] quicksort [ 7 ]
+-- eight run biggerSorted becomes :
+-- [ 8, 6, 5, 4, 7 ]
+--
+-- smallerSorted [2, 1] 
+-- is then prepended to [3] and appended to biggerSorted
+-- [8, 6, 5, 4, 7] to form [2, 1] ++ [3] ++ [8, 6, 5, 4, 7]
+-- smallerSorted is then sorted becoming [1,2]
+-- biggerSorted is then sorted becoming [4,5,6,7,8]
+-- again smallersorted is prepended to [3] and biggerSorted
+-- is appended to it forming the final list of :
+-- [1,2,3,4,5,6,7,8]
+
+-- 2b)
+-- foldr (++) [] [[1,2], [3,4], [5,6]]
+-- ++ [1,2]( ++ [3,4] ( ++ [5,6] [] ))
+-- [[1,2], [3,4]] ++ [5,6]
+-- [[1,2]] ++ [3,4,5,6]
+-- [1,2,3,4,5,6]
+--
+-- foldl  (++)  []  [[1,2], [3,4], [5,6]]
+-- ++ ( ++ ( ++ [] [1,2] ) [3,4] ) [5,6] 
+-- [1,2] ++ [[3,4],[5,6]]
+-- [1,2,3,4] ++ [[5,6]]
+-- [1,2,3,4,5,6]
+
+-- 3a)
+--myunzip :: [(a, b)] -> ([a], [b])
+--myunzip [] = ([],[])
+--myunzip ((x,y):xs) = map xs ([x] ++ fst xs, [y] ++ snd xs )
+	--let
+	--	(a,b) = myunzip xs
+	--in
+	--	([x] ++ a, [y] ++ b)
+
+-- 3b)
+lettersOnly  ::  String -> String
+lettersOnly  "" = "Empty"
+lettersOnly (x:xs) = filter ( `elem` ['a'..'z']) (x:xs)
